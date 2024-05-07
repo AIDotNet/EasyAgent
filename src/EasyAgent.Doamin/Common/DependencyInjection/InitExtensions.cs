4453bb196bfa29079ca6d8619b5b2c186e81ff5e
zyxucp <286513187@qq.com>
2024-05-07 21:08:09 +0800

    add 目录层级

---
+++ src/EasyAgent.Doamin/Common/DependencyInjection/InitExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using SqlSugar;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyAgent.Doamin.Common.DependencyInjection
+{
+    public static class InitExtensions
+    {
+        /// <summary>
+        /// 使用codefirst创建数据库表
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static WebApplication CodeFirst(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+           
+            }
+            return app;
+        }
+    }
+}
