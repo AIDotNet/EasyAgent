20fa4b6b1e09d98704c5f2fce8fa5ed326878f97
zyxucp <286513187@qq.com>
2024-05-07 21:19:44 +0800

    add db

---
+++ src/EasyAgent.Doamin/Common/Map/AutoMapProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace EasyAgent.Doamin.Common.Map
+{
+    public class AutoMapProfile : Profile
+    {
+        public AutoMapProfile()
+        {
+            //CreateMap<PMP_BizCase_Main_Max, BizcaseQueryDTO>();
+        }
+    }
+}
---
+++ src/EasyAgent.Doamin/Options/DBConnectionOption.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyAgent.Doamin.Options
+{
+    public class DBConnectionOption
+    {
+        /// <summary>
+        /// sqlite连接字符串
+        /// </summary>
+        public static string DbType { get; set; }
+        /// <summary>
+        /// pg链接字符串
+        /// </summary>
+        public static string ConnectionStrings { get; set; }
+    }
+}
